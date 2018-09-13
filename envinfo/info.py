ef26d1a33f3e693f0a83fd8b1b5ddd3f318ac5d1
mrstephenneal <stephen.neal14@gmail.com>
2018-09-13 12:25:28 -0400

    Moved Formatter class into format.py

--- envinfo/info.py
+++ envinfo/info.py
@@ -20,66 +20,7 @@ import multiprocess as mp
 from os import getlogin
 from psutil import virtual_memory
 from platform import python_version, python_compiler, system, release, machine, processor, architecture, node
-
-
-class Formatter:
-    """Inherited class with methods called by numerous classes."""
-    def __init__(self, info_type, info):
-        self.type = info_type
-        self.info = info
-
-    def __str__(self):
-        return self.create_string()
-
-    def create_string(self):
-        lst = [self.type, '------------------']
-        for k, v in self.info().items():
-            lst.append(self.format_string(k, v))
-        lst.append('\n')
-        return '\n'.join(lst)
-
-    @staticmethod
-    def format_string(key, value):
-        return f'{key:20} ==> {value}'
-
-    @staticmethod
-    def format_size(num_bytes, binary=False, strip=True):
-        """
-        Format a number of bytes as a human readable size.
-
-        Parameters
-        ----------
-        num_bytes : int
-            The size to format.
-        binary : bool, optional
-            The base to group the number of bytes.
-        strip : bool, optional
-            If trailing zeros should be keeped or stripped.
-
-        Returns
-        -------
-        str
-            The human readable file size.
-        """
-        size_units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-
-        if binary:
-            base = 2 ** 10
-        else:
-            base = 10 ** 3
-
-        for i, unit in reversed(list(enumerate(size_units))):
-            divider = base ** i
-            if num_bytes >= divider:
-                formatted = '{:0.2f}'.format(num_bytes / divider, unit)
-                if strip:
-                    formatted = formatted.rstrip('0').rstrip('.')
-                formatted = '{} {}'.format(formatted, unit)
-
-                return formatted
-
-        # Failed to match a unit
-        return '0 {}'.format(size_units[0])
+from .format import Formatter
 
 
 class Python(Formatter):
