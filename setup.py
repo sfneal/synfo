ec5adde8f75f7592c3fce84632e26d072684e9b4
mrstephenneal <stephen.neal14@gmail.com>
2018-09-13 12:30:30 -0400

    Added setup.py and version

---
+++ setup.py
@@ -0,0 +1,30 @@
+import re
+from setuptools import setup, find_packages
+
+
+# Retrieve version number
+VERSIONFILE = "envinfo/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+
+
+setup(
+    name='envinfo',
+    version=verstr,
+    packages=find_packages,
+    install_requires=[
+        'platform',
+        'psutil',
+        'multiprocess'
+    ],
+    url='https://github.com/mrstephenneal/envinfo',
+    license='GPL-3.0',
+    author='Stephen Neal',
+    author_email='stephen@stephenneal.net',
+    description='A lightweight utility for retrieving system information and specifications.'
+)
