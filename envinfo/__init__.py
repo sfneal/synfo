f3d2fdd6403f5ebbe7587c0bbb010b691e594db5
mrstephenneal <stephen.neal14@gmail.com>
2018-09-25 15:29:27 -0400

    Added call to EnvInfo

--- envinfo/__init__.py
+++ envinfo/__init__.py
@@ -6,6 +6,6 @@ __all__ = ['EnvInfo', 'Packages']
 
 
 # Import and add Drives to __all__ declaration if running on Windows
-if EnvInfo.system.os == 'Windows':
+if EnvInfo().system.os == 'Windows':
     from envinfo.drives import Drives
     __all__.append('Drives')
