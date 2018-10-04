cf2d67f7e1aaceac7ef47e6d1e7524c0bff59042
mrstephenneal <stephen.neal14@gmail.com>
2018-10-04 12:28:25 -0400

    Deleted modules and modified setup.py to be a wrapper module

--- setup.py
+++ setup.py
@@ -1,33 +1,18 @@
-import re
-from setuptools import setup, find_packages
-
-
-# Retrieve version number
-VERSIONFILE = "envinfo/_version.py"
-verstrline = open(VERSIONFILE, "rt").read()
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-mo = re.search(VSRE, verstrline, re.M)
-if mo:
-    verstr = mo.group(1)
-else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE))
+import os
+from setuptools import setup
 
 
 setup(
     name='envinfo',
-    version=verstr,
-    packages=find_packages(),
+    version='1.5.1',
     install_requires=[
-        'multiprocess'
+        'synfo'
     ],
-    entry_points={
-        'console_scripts': [
-            'envinfo = envinfo.system:main'
-        ]
-    },
-    url='https://github.com/mrstephenneal/envinfo',
+    url='https://github.com/mrstephenneal/synfo',
     license='GPL-3.0',
     author='Stephen Neal',
     author_email='stephen@stephenneal.net',
-    description='A lightweight utility for retrieving system information and specifications.'
+    description='Legacy wrapper module for synfo.'
 )
+
+os.system('pip uninstall envinfo')
