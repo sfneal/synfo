9e84f3e64e86706781015156a0aabbe76dd0537c
mrstephenneal <stephen.neal14@gmail.com>
2019-02-25 12:26:45 -0500

    ADD singular demo file

---
+++ demos/demo.py
@@ -0,0 +1,9 @@
+from synfo import Synfo
+
+
+# Retrieve information about Python version and compiler
+print(Synfo().python)
+
+
+# Retrieve information about Python version and compiler
+print(Synfo().system)
